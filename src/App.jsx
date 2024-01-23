28d119146ed73daf36e164f7b3a810c9ffacc7e8
Sharan Suri <31404638+Sharan420@users.noreply.github.com>
2024-01-23 21:11:44 +0530

    Styled Scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { Navbar } from './Components'
 
 function App() {
   return (
-    <>
+    <div className="absolute h-screen overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-gray-500 ">
       <Navbar />
       <Intro />
       <About />
-    </>
+    </div>
   )
 }
 
